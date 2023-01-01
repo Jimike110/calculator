64f66797a27b37a8fd629810d35c7d537132ffb2
Jimike110 <oladoyemike@gmail.com>
2023-01-01 22:32:33 +0100

    A simple calculator. Complete with arithmetic operations, a dot and a backspace button.

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function clearScreen(){
+	document.querySelector("#result").value="";
+}
+function display(value){
+	document.querySelector("#result").value += value;
+}
+function calculate(){
+	var p= document.querySelector("#result").value;
+	var q= eval(p);
+	document.querySelector("#result").value = q;
+}
+function backspace(){
+	document.querySelector("#result").value = document.querySelector("#result").value.substring(0, document.querySelector("#result").value.length-1);
+}
