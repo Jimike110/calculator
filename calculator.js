b6520d8a13e0cbd44cff2e052903a72d72f5959f
Jimike110 <oladoyemike@gmail.com>
2023-01-02 11:00:59 +0100

    Formatted Document

--- calculator.js
+++ calculator.js
@@ -1,14 +1,14 @@
-function clearScreen(){
-	document.querySelector("#result").value="";
+function clearScreen() {
+	document.querySelector("#result").value = "";
 }
-function display(value){
+function display(value) {
 	document.querySelector("#result").value += value;
 }
-function calculate(){
-	var p= document.querySelector("#result").value;
-	var q= eval(p);
+function calculate() {
+	var p = document.querySelector("#result").value;
+	var q = eval(p);
 	document.querySelector("#result").value = q;
 }
-function backspace(){
-	document.querySelector("#result").value = document.querySelector("#result").value.substring(0, document.querySelector("#result").value.length-1);
+function backspace() {
+	document.querySelector("#result").value = document.querySelector("#result").value.substring(0, document.querySelector("#result").value.length - 1);
 }
